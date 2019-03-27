fab50489dd87488b6e02a5c1fd00d649e35b3387
a912892942 <912892942@qq.com>
2019-03-27 11:50:37 +0800

    整合头尾中间

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-// Vue.config.productionTip = false
+import VueRouter from 'vue-router'
+import  './assets/css/style.css'
+Vue.use(VueRouter)
+import index from './components/index.vue'
+// Vue.config.productionTip = falses
+let routes = [{path:'/',component:index},{path:'/index',component:index}]
+let router = new VueRouter({
+  routes
+})
 
 new Vue({
   el:'#app',
   render: h => h(App),
+  router
 })
