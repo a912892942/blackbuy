c411ec6977f791496fa4e1adfe35e3cfa2df47e7
a912892942 <912892942@qq.com>
2019-03-27 14:51:56 +0800

    购物车功能

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import  './assets/css/style.css'
-Vue.use(VueRouter)
-import index from './components/index.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import "./assets/css/style.css";
+Vue.use(VueRouter);
+import index from "./components/index.vue";
+import cart from "./components/cart.vue";
 // Vue.config.productionTip = falses
-let routes = [{path:'/',component:index},{path:'/index',component:index}]
+let routes = [
+  { path: "/", component: index },
+  { path: "/index", component: index },
+  { path: "/cart", component: cart }
+];
 let router = new VueRouter({
   routes
-})
+});
 
 new Vue({
-  el:'#app',
+  el: "#app",
   render: h => h(App),
   router
-})
+});
